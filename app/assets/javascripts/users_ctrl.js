9aa85b6279db5b0af45fa24edfd74471e488f6d6
Madison Maloof <maddymaloof@gmail.com>
2016-04-27 21:32:57 -0500

    changed link placement, started working with users_ctrl.js and angular

--- app/assets/javascripts/users_ctrl.js
+++ app/assets/javascripts/users_ctrl.js
@@ -1,1 +1,10 @@
 /* global angular */
+(function() {
+  angular.module('app').controller('usersCtrl', function($scope, $http) {
+    $scope.setup = function() {
+      $http.get('/api/v1/users.json').then(function(response) {
+        console.log(response);
+      });
+    };
+  });
+})();
